ddaefe0d70e9bf0016838177de593a1dbfd2907b
fawcat0906 <fawcat0906@gmail.com>
2019-09-18 16:21:28 +0800

    Modify HM to global variable

--- HS_ARG.h
+++ HS_ARG.h
@@ -1,8 +1,13 @@
+#include <vector>
 #ifndef GA_ARG_H
 #define GA_ARG_H
-
 #define HMCR 0.95
 #define PAR 0.1
 #define HMS 5
 #define columnNum 3
+
+typedef vector<int> vec_1D;
+typedef vector<vector<int>> vec_2D;
+
+vec_2D HM(HMS, vec_1D(columnNum, 0));
 #endif
